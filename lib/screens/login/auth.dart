675ca5bbcf9cc0442656fc0eee3db46b065481d4
ThatXuxe <obuoy1804@gmail.com>
2020-08-11 23:06:22 +0530

    More colorful auth landing page Add http dependency

--- lib/screens/login/auth.dart
+++ lib/screens/login/auth.dart
@@ -1,103 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:procrastinay/util/inputWidget.dart';
+
+import '../background.dart';
 
 class Auth extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //todo redirect to /home if already signed in
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Login or Register"),
-        ),
+        resizeToAvoidBottomPadding: false,
         backgroundColor: Theme.of(context).backgroundColor,
-        body: new Container(
-            decoration:
-                new BoxDecoration(color: Theme.of(context).backgroundColor),
-            child: Padding(
-                padding: EdgeInsets.all(10),
-                child: ListView(
-                  children: <Widget>[
-                    Container(
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.all(10),
+        body: Stack(children: <Widget>[
+          Background(),
+          Column(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.only(top: 180)),
+              Center(
+                  child: Text(
+                'Procrastinay',
+                textDirection: TextDirection.ltr,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontFamily: 'Times New Roman', fontSize: 47),
+              )),
+              Padding(
+                padding: EdgeInsets.only(top: 150),
+              ),
+              Column(
+                children: <Widget>[
+                  ///holds email header and inputField
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 40, bottom: 10),
                         child: Text(
-                          'Procrastinay',
-                          style: TextStyle(
-                              color: Theme.of(context).accentColor,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 30),
-                        )),
-                    Container(
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'Log in',
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: Theme.of(context).accentColor),
-                        )),
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      child: TextField(
-                        //controller: nameController,
-                        decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context).accentColor,
-                                  width: 50.0)),
-                          labelText: 'User Name',
+                          "Email",
+                          style:
+                              TextStyle(fontSize: 16, color: Color(0xFF999A9A)),
                         ),
                       ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                      child: TextField(
-                        obscureText: true,
-                        //controller: passwordController,
-                        decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context).accentColor,
-                                  width: 50.0)),
-                          labelText: 'Password',
-                        ),
+                      Stack(
+                        alignment: Alignment.bottomRight,
+                        children: <Widget>[
+                          EmailInputWidget(30.0, 0.0),
+                        ],
                       ),
-                    ),
-                    FlatButton(
-                      onPressed: () {
-                        //todo forgot password screen
-                      },
-                      textColor: Theme.of(context).buttonColor,
-                      child: Text('Forgot Password'),
-                    ),
-                    Container(
-                        height: 50,
-                        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                        child: RaisedButton(
-                          textColor: Colors.white,
-                          color: Theme.of(context).buttonColor,
-                          child: Text('Login'),
-                          onPressed: () {
-                            //todo login
-                          },
-                        )),
-                    Container(
-                        child: Row(
-                      children: <Widget>[
-                        Text('Do not have an account?'),
-                        FlatButton(
-                          textColor: Theme.of(context).buttonColor,
-                          child: Text(
-                            'Register',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/register');
-                          },
-                        )
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.center,
-                    ))
-                  ],
-                ))));
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 50),
+                  ),
+                  roundedRectButton(
+                      "Let's get Started", signInGradients, false),
+                  roundedRectButton(
+                      "Create an Account", signUpGradients, false),
+                ],
+              )
+            ],
+          ),
+        ]));
   }
 }
+
+Widget roundedRectButton(
+    String title, List<Color> gradient, bool isEndIconVisible) {
+  return Builder(builder: (BuildContext mContext) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: Stack(
+        alignment: Alignment(1.0, 0.0),
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            width: MediaQuery.of(mContext).size.width / 1.7,
+            decoration: ShapeDecoration(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)),
+              gradient: LinearGradient(
+                  colors: gradient,
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight),
+            ),
+            child: Text(title,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500)),
+            padding: EdgeInsets.only(top: 16, bottom: 16),
+          ),
+          Visibility(
+            visible: isEndIconVisible,
+            child: Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: ImageIcon(
+                  AssetImage("assets/ic_forward.png"),
+                  size: 30,
+                  color: Colors.white,
+                )),
+          ),
+        ],
+      ),
+    );
+  });
+}
+
+const List<Color> signInGradients = [
+  Color(0xFF0EDED2),
+  Color(0xFF03A0FE),
+];
+
+const List<Color> signUpGradients = [
+  Color(0xFFFF9945),
+  Color(0xFFFc6076),
+];
