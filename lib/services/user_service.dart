76061ccd530b7b8f45f806334ae51b25ce646d31
ThatXuxe <obuoy1804@gmail.com>
2020-08-14 01:11:02 +0530

    fix typo and local file import

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -2,11 +2,10 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:procrastinay/config.dart';
-import 'package:procrastinay/models/api_reponse.dart';
+import 'package:procrastinay/models/api_response.dart';
+import 'package:procrastinay/models/user/create_user.dart';
 import 'package:procrastinay/models/user/login_user.dart';
 import 'package:procrastinay/models/user_token.dart';
-
-import 'file:///D:/Mobile_Workspace/procrastinay/lib/models/user/create_user.dart';
 
 class UserService {
   static const headers = {'Content-Type': 'application/json'};
