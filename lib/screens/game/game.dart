f7d1bad902e55a1d407b75972ee4e2e4722694ad
Minhyung Lee <2048nim@gmail.com>
2020-08-14 17:49:53 +0800

    added svg images

--- lib/screens/game/game.dart
+++ lib/screens/game/game.dart
@@ -1,10 +1,23 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:procrastinay/models/api_response.dart';
+import 'package:procrastinay/models/user_classtype.dart';
 import 'package:zerker/zerker.dart';
+import 'dart:convert';
+import 'package:procrastinay/config.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(Game());
 
 class Game extends StatelessWidget {
+  Future<String> requestClass() async {
+    String response = (await http.get(Config.API_LINK + '/users/me',
+        headers: {'class': 'application/json'})) as String;
+
+    print(response);
+    return (response);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
